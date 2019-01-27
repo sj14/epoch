a4f2d8c23c504ea1f3346443e9e61490a931036d
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2019-01-27 21:39:18 +0100

    Added "2019-01-25 21:51:38" Format

--- main.go
+++ main.go
@@ -112,58 +112,93 @@ func fromTimestamp(timestamp int64, typ tsType) time.Time {
 }
 
 func fromFormatted(input string) (time.Time, error) {
+	// "Mon, 02 Jan 2006 15:04:05 MST"
 	if t, err := time.Parse(time.RFC1123, input); err == nil {
 		return t, nil
 	}
+
+	// "Mon, 02 Jan 2006 15:04:05 -0700"
 	if t, err := time.Parse(time.RFC1123Z, input); err == nil {
 		return t, nil
 	}
+
+	// "2006-01-02T15:04:05Z07:00"
 	if t, err := time.Parse(time.RFC3339, input); err == nil {
 		return t, nil
 	}
+
+	// "2006-01-02T15:04:05.999999999Z07:00"
 	if t, err := time.Parse(time.RFC3339Nano, input); err == nil {
 		return t, nil
 	}
+
+	// "02 Jan 06 15:04 MST"
 	if t, err := time.Parse(time.RFC822, input); err == nil {
 		return t, nil
 	}
+
+	// "02 Jan 06 15:04 -0700"
 	if t, err := time.Parse(time.RFC822Z, input); err == nil {
 		return t, nil
 	}
+
+	// "Monday, 02-Jan-06 15:04:05 MST"
 	if t, err := time.Parse(time.RFC850, input); err == nil {
 		return t, nil
 	}
+
+	// "Mon Jan _2 15:04:05 2006"
 	if t, err := time.Parse(time.ANSIC, input); err == nil {
 		return t, nil
 	}
+
+	// "Mon Jan _2 15:04:05 MST 2006"
 	if t, err := time.Parse(time.UnixDate, input); err == nil {
 		return t, nil
 	}
+
+	// "Mon Jan 02 15:04:05 -0700 2006"
 	if t, err := time.Parse(time.RubyDate, input); err == nil {
 		return t, nil
 	}
+
+	// "3:04PM"
 	if t, err := time.Parse(time.Kitchen, input); err == nil {
 		return t, nil
 	}
+
+	// "Jan _2 15:04:05"
 	if t, err := time.Parse(time.Stamp, input); err == nil {
 		return t, nil
 	}
-	if t, err := time.Parse(time.StampMicro, input); err == nil {
-		return t, nil
-	}
+
+	// "Jan _2 15:04:05.000"
 	if t, err := time.Parse(time.StampMilli, input); err == nil {
 		return t, nil
 	}
+
+	// "Jan _2 15:04:05.000000"
+	if t, err := time.Parse(time.StampMicro, input); err == nil {
+		return t, nil
+	}
+
+	// "Jan _2 15:04:05.000000000"
 	if t, err := time.Parse(time.StampNano, input); err == nil {
 		return t, nil
 	}
+
+	// "Mon, 02 Jan 2006 15:04:05 GMT"
 	if t, err := time.Parse(http.TimeFormat, input); err == nil {
 		return t, nil
 	}
 
 	// handle Go's default time.Now() format (e.g. 2019-01-26 09:43:57.377055 +0100 CET m=+0.644739467)
-	const defaultNow = "2006-01-02 15:04:05.999999999 -0700 MST"
-	if t, err := time.Parse(defaultNow, strings.Split(input, " m=")[0]); err == nil {
+	if t, err := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", strings.Split(input, " m=")[0]); err == nil {
+		return t, nil
+	}
+
+	// "2019-01-25 21:51:38"
+	if t, err := time.Parse("2006-01-02 15:04:05.999999999", input); err == nil {
 		return t, nil
 	}
 
